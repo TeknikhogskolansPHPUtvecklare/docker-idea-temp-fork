76b943b691ba1dfd8ed1a25afdb1c12da4950f24
Ironboy <ironboy@users.noreply.github.com>
2022-05-25 12:26:03 +0200

    nice

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 echo 'Hello and welcome to this Apache + PHP environment!<br><br>';
 
-echo 'We are running '.apache_get_version().'with PHP '.phpversion().'<br><br>';
+echo 'We are running '.apache_get_version().' with PHP '.phpversion().'<br><br>';
 
-echo '<a href="/php-info.php">Here is some info about PHP-settings</a>';
+echo '<a href="/php-info.php">Here is more info about the PHP settings</a>';
