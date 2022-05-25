847565d1c57610c929b7aa9a0f9e35b2cc51c666
Ironboy <ironboy@users.noreply.github.com>
2022-05-25 12:19:49 +0200

    more php syntax

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-echo 'Hello and welcome to this Apache 2 + PHP 8 environment!';
-echo '<a href="/phpinfo.php">Here is some info about PHP-settings</a>';
+
+echo 'Hello and welcome to this Apache 2 + PHP 8 environment!<br><br>';
+
+echo '<a href="/php-info.php">Here is some info about PHP-settings</a>';
