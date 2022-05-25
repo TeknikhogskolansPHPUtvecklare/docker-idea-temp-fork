585c6a0f541f9fcf4f169889c2b83cdfdf709a9e
Ironboy <ironboy@users.noreply.github.com>
2022-05-25 12:24:58 +0200

    compacter

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 echo 'Hello and welcome to this Apache + PHP environment!<br><br>';
 
-echo 'Apache version:'.apache_get_version().'<br><br>';
-
-echo 'PHP version:'.phpversion().'<br><br>';
+echo 'We are running '.apache_get_version().'with PHP '.phpversion().'<br><br>';
 
 echo '<a href="/php-info.php">Here is some info about PHP-settings</a>';
