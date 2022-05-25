e0fdd338f000e5ce4036c015abc6d4c95063d233
Ironboy <ironboy@users.noreply.github.com>
2022-05-25 12:22:31 +0200

    more fuun

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'Hello and welcome to this Apache + PHP environment!<br><br>';
+
+echo 'Apache version:'.apache_get_version().'<br><br>';
+
+echo 'PHP version:'.phpversion().'<br><br>';
+
+echo '<a href="/php-info.php">Here is some info about PHP-settings</a>';
