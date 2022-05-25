af33ede953837607a9cd54ff3e46940f542516d6
Ironboy <ironboy@users.noreply.github.com>
2022-05-25 12:23:16 +0200

    php and apache version

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo 'Hello and welcome to this Apache 2 + PHP 8 environment!<br><br>';
+echo 'Hello and welcome to this Apache + PHP environment!<br><br>';
+
+echo 'Apache version:'.apache_get_version().'<br><br>';
+
+echo 'PHP version:'.phpversion().'<br><br>';
 
 echo '<a href="/php-info.php">Here is some info about PHP-settings</a>';
